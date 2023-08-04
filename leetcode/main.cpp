3258d43acfb730d3485c5e3142c8a795572b4f9e
Karapetyan Artem <agkarapetyan_1@edu.hse.ru>
2023-08-05 03:59:07 +0600

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Нелли Карапетян on 05.08.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
